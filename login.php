2fd25e803170b9af6127633f7cf1608e62951e0d
madhura-pawar <madhurapawar091@gmail.com>
2025-12-17 19:01:16 +0530

    Update login.php

--- login.php
+++ login.php
@@ -36,26 +36,40 @@ if (isset($_POST['login'])) {
 
 <!DOCTYPE html>
 <html>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 <head>
     <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<link rel="stylesheet" href="../css/dashboard.css">
 </head>
 <body>
 
-<h2>Login</h2>
-
-<?php
-if ($message != "") {
-    echo "<p style='color:red;font-weight:bold;'>$message</p>";
-}
-?>
-
-<form method="POST">
+<div class="container mt-5">
+  <div class="row justify-content-center">
+    <div class="col-md-4">
+      <div class="card shadow">
+        <div class="card-body">
+          <?php if (!empty($message)) { ?>
+  <div class="alert alert-danger text-center">
+    <?php echo $message; ?>
+  </div>
+<?php } ?>
+         <form method="POST">
     <input type="email" name="email" placeholder="Email" required><br><br>
     <input type="password" name="password" placeholder="Password" required><br><br>
     <button type="submit" name="login">Login</button>
-</form>
+</form> 
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
 
 <a href="register.php">Create New Account</a>
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
+           
+    
